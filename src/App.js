b0d505820593c83a1760754d3564cd05deed0455
Matt Tingen <matt.tingen@gmail.com>
2017-11-09 22:38:47 -0500

    Add css modules

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div styleName="App">
+        <header styleName="header">
+          <img src={logo} styleName="logo" alt="logo" />
+          <h1 styleName="title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p styleName="intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
